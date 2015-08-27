d9af743eec309a260def6f62611397322c9f0742
Michael Venezia <mike@cc.com>
2015-08-27 09:22:35 -0400

    changing images

--- files/docker/website-a/v1/php/website/index.php
+++ files/docker/website-a/v1/php/website/index.php
@@ -2,7 +2,7 @@
 
 echo '
 <html><head><title>Success!</title></head>
-<body><img src="colbert.gif" /><br/>
+<body><img src="' . rand(1,3) . '.jpg" style="max-width: 80%" /><br/>
 <p>Running on 
 ' . php_uname("a") . 
 '
