9773cd3f6297f4d52a01e2878114d7f1ec8c26e9
Michael Venezia <michael.venezia@viacom.com>
2015-08-27 13:00:40 +0200

    derp

--- files/commands/create-etcd.sh
+++ files/commands/create-etcd.sh
@@ -2,9 +2,9 @@
 
 aws ec2 run-instances \
     --image-id ami-a7dd02cc \
-    --subnet-id subnet-05411f5c \
-    --security-group-ids sg-cd1a6aaa \
-    --key-name venezia-training \
+    --subnet-id <subnet id> \
+    --security-group-ids <security group id> \
+    --key-name <key id> \
     --region us-east-1 \
     --instance-type m3.medium \
     --associate-public-ip-address \
