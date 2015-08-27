e8e77060b1afe23b6f65ca48f305c9e7a7901424
Michael Venezia <michael.venezia@viacom.com>
2015-08-27 12:57:57 +0200

    adding etcd.yaml

--- files/commands/create-etcd.sh
+++ files/commands/create-etcd.sh
@@ -2,10 +2,10 @@
 
 aws ec2 run-instances \
     --image-id ami-a7dd02cc \
-    --subnet-id <subnet-id-placeholder> \
-    --security-group-ids <sg-id-placeholder> \
-    --key-name <ssh-keypair-placeholder> \
-    --region <region-placeholder> \
+    --subnet-id subnet-05411f5c \
+    --security-group-ids sg-cd1a6aaa \
+    --key-name venezia-training \
+    --region us-east-1 \
     --instance-type m3.medium \
     --associate-public-ip-address \
     --user-data file://../cloud-configs/etcd.yaml
